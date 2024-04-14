0702c81ccfa6640706612484ba716b7e6a0c27a4
JHU-UCSD <166658372+JHU-UCSD@users.noreply.github.com>
2024-04-14 13:27:10 -0400

    Update market_participant.py

--- market_participant.py
+++ market_participant.py
@@ -1,6 +1,8 @@
 import numpy as np
 import json
 import argparse
+import datetime
+from itertools import accumulate
 import da_offers as da
 # import rt_offers as rt
 
@@ -13,27 +15,38 @@ class NpEncoder(json.JSONEncoder):
         if isinstance(obj, np.ndarray):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
+    
+######################################################### Default offers ########################################################
+normal_charge_mc = 2
+normal_charge_mq = 100
+
+normal_discharge_mc = 30
+normal_discharge_mq = 100
+
+extreme_charge_mc = 1
+extreme_charge_mq = 10
+
+extreme_discharge_mc = 50
+extreme_discharge_mq = 10
 
 ########################################################## Input Data ###########################################################
-#################################################################################################################################
-if __name__ == '__main__':
-    # Add argument parser for three required input arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('time_step', type=int, help='Integer time step tracking the progress of the\
-                        simulated market.')
-    parser.add_argument('market_file', help='path to json formatted dictionary with market \
-                        information.')
-    parser.add_argument('resource_file', help='path to json formatted dictionary with resource \
-                        information.')
+# Add argument parser for three required input arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('time_step', type=int, help='Integer time step tracking the progress of the\
+                    simulated market.')
+parser.add_argument('market_file', help='path to json formatted dictionary with market \
+                    information.')
+parser.add_argument('resource_file', help='path to json formatted dictionary with resource \
+                    information.')
 
-    args = parser.parse_args()
+args = parser.parse_args()
 
-    # Parse json inputs into python dictionaries
-    time_step = args.time_step
-    with open(args.market_file, 'r') as f:
-        market_info = json.load(f)
-    with open(args.resource_file, 'r') as f:
-        resource_info = json.load(f)
+# Parse json inputs into python dictionaries
+time_step = args.time_step
+with open(args.market_file, 'r') as f:
+    market_info = json.load(f)
+with open(args.resource_file, 'r') as f:
+    resource_info = json.load(f)
 
 # Extract resources rid
 rid = resource_info['rid']
@@ -47,14 +60,19 @@ market_type = market_info['market_type']
 prices_forecast = market_info['previous'][market_type]['prices']['EN'][bus_value]
 total_period = len(prices_forecast)
 
-########################################################## Make Offers ##########################################################
-#################################################################################################################################
+########################################### Day-ahead Market ############################################
 if 'DAM' in market_type:
     prices_forecast = market_info['previous'][market_type]['prices']['EN'][bus_value]
     required_times = [t for t in market_info['timestamps']]
+    price_dict = {required_times[i]:prices_forecast[i] for i in range(len(required_times))}
+    # Writing prices to a local JSON file
+    file_path = "da_prices.json"
+    with open(file_path, "w") as file:
+        json.dump(price_dict, file)
+    prices = np.array(prices_forecast)
 
     # Make the offer curves and unload into arrays
-    offer_dis, offer_ch = da.da_offers(prices_forecast,bus_value,initial_energy)
+    offer_dis, offer_ch = da.da_offers(prices_forecast,initial_energy,bus_value,rid)
 
     charge_mc = offer_ch['BinStart'].values
     charge_mq = offer_ch['ChValue'].values
@@ -75,15 +93,13 @@ if 'DAM' in market_type:
                 list_charge_mc.append(charge_mc[i])
                 list_charge_mq.append(charge_mq[i])
         if len(list_charge_mc) == 0:
-            list_charge_mc.append(-100)
-            list_charge_mq.append(10)
+            list_charge_mc.append(extreme_charge_mc)
+            list_charge_mq.append(extreme_charge_mq)
         block_ch_mc[required_times[t]] = list_charge_mc
         block_ch_mq[required_times[t]] = list_charge_mq
 
     block_dc_mc = {}
     block_dc_mq = {}
-    block_soc_mq = {}
-    block_soc_mc = {}
     for t in range(total_period):
         list_discharge_mc = []
         list_discharge_mq = []
@@ -92,10 +108,14 @@ if 'DAM' in market_type:
                 list_discharge_mc.append(discharge_mc[i])
                 list_discharge_mq.append(discharge_mq[i])
         if len(list_discharge_mc) == 0:
-            list_discharge_mc.append(100)
-            list_discharge_mq.append(10)
+            list_discharge_mc.append(extreme_discharge_mc)
+            list_discharge_mq.append(extreme_discharge_mc)
         block_dc_mc[required_times[t]] = list_discharge_mc
         block_dc_mq[required_times[t]] = list_discharge_mq
+
+    block_soc_mq = {}
+    block_soc_mc = {}
+    for t in range(total_period):
         block_soc_mc[required_times[t]] = 0
         block_soc_mq[required_times[t]] = 0
 
@@ -127,7 +147,6 @@ if 'DAM' in market_type:
     constants['bid_soc'] = False
 
     # Pacakge the dictionaries into an output formatted dictionary
-    # rid = 'R00229'
     offer_out_dict = {rid:{}}
     offer_out_dict[rid] = {"block_ch_mc":block_ch_mc, "block_ch_mq":block_ch_mq, "block_dc_mc":block_dc_mc, "block_dc_mq":block_dc_mq, "block_soc_mc":block_soc_mc, "block_soc_mq":block_soc_mq}
     offer_out_dict[rid].update(rgu_dict)
@@ -140,19 +159,75 @@ if 'DAM' in market_type:
 
 ########################################### Real-time Market ############################################
 elif 'RTM' in market_type:
+    price_path = "da_prices.json"
+    with open(price_path, "r") as file:
+        prices = json.load(file)
+        dam_times = [key for key in prices.keys()]
+        prices = [value for value in prices.values()]
+    # Read in information from the resource
+    en_schedule_list = [z[0] for z in resource_info["ledger"][rid]["EN"].values()]
+    sch_time = [z for z in resource_info["ledger"][rid]["EN"].keys()]
+    initial_soc = resource_info["status"][rid]["soc"]
+    marginal_quantities, marginal_prices = zip(*en_schedule_list)
+    adjusted_marginal_quantities = [initial_soc] + list(marginal_quantities)
+    # Calculating the cumulative sum of adjusted marginal quantities
+    cumulative_soc = list(accumulate(adjusted_marginal_quantities))
+    #print("cumulative soc is", cumulative_soc)
+    # Generating a new list of cumulative soc and marginal prices
+    soc_price_list = list(zip(cumulative_soc, marginal_prices))
+    #print(soc_price_list)
     required_times = [t for t in market_info['timestamps']]
+    # Convert the offer curves to timestamp:offer_value dictionaries
+    #soc_mq =soc_price_list.values
+    soc_mq =cumulative_soc
+    soc_mc =prices
+    time_soc = np.zeros(len(required_times))
+    price_soc = np.zeros(len(required_times))
+    soc = np.zeros(len(required_times))
+    ti =0
+    idx_price = 0
+    for i in range(len(required_times)):
+        # real-time datetime format
+        dt1 = datetime.datetime.strptime(required_times[i], '%Y%m%d%H%M')
+        # resource json file datetime format
+        dt2 = datetime.datetime.strptime(sch_time[ti], '%Y%m%d%H%M')
+        dt3 = datetime.datetime.strptime(dam_times[idx_price], '%Y%m%d%H%M')
+        while dt1 > dt3 and dt1.hour != dt3.hour:
+            idx_price += 1
+            dt3 = datetime.datetime.strptime(dam_times[idx_price], '%Y%m%d%H%M')
+        if idx_price == len(dam_times)-1:
+            price_soc[i] = 0
+        else:
+            if marginal_quantities[ti]>0:
+                multiplier = 1
+            else:
+                multiplier = -1
+            price_soc[i] = prices[idx_price+1]*multiplier
+
+        while dt1 > dt2 and dt1.hour != dt2.hour:
+            ti += 1
+            dt2 = datetime.datetime.strptime(sch_time[ti], '%Y%m%d%H%M')
+
+        if dt1.hour == dt2.hour:
+            soc[i] = soc_mq[ti+1]
+        elif ti == 0:
+            soc[i] = initial_soc
+        else:
+            soc[i] = soc_mq[ti]
+
     block_soc_mq = {}
     for i, soc_mq in enumerate(required_times):
-        block_soc_mq[required_times[i]] = 1
+        block_soc_mq[required_times[i]] = float(soc[i])
     block_soc_mc = {}
     for i, soc_mc in enumerate(required_times):
-        block_soc_mc[required_times[i]] = 100
+        block_soc_mc[required_times[i]] = float(price_soc[i])
 
     # Convert the offer curves to timestamp:offer_value dictionaries
-    charge_mc = np.zeros(36)
-    charge_mq = np.zeros(36)
-    discharge_mc = np.zeros(36)
-    discharge_mq = np.zeros(36)
+    charge_mc = np.zeros(total_period)
+    charge_mq = np.zeros(total_period)
+    discharge_mc = np.zeros(total_period)
+    discharge_mq = np.zeros(total_period)
+
     block_ch_mc = {}
     for i, cost in enumerate(charge_mc):
         block_ch_mc[required_times[i]] = 0
@@ -198,7 +273,6 @@ elif 'RTM' in market_type:
     constants['bid_soc'] = True
 
     # Pacakge the dictionaries into an output formatted dictionary
-    # rid = 'R00229'
     offer_out_dict = {rid:{}}
     offer_out_dict[rid] = {"block_ch_mc":block_ch_mc, "block_ch_mq":block_ch_mq, "block_dc_mc":block_dc_mc, "block_dc_mq":block_dc_mq, "block_soc_mc":block_soc_mc, "block_soc_mq":block_soc_mq}
     offer_out_dict[rid].update(rgu_dict)
